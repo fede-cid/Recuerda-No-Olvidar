48dcb1749586d51deb5c7c0d4bac36d50eb64151
fede-cid <fede.cid.96@gmail.com>
2022-09-28 23:17:31 -0300

    first commit

---
+++ app-de-tareas/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import ListaDeTareas from './js/listaDeTareas'
+
+function App() {
+  return (
+    <div className="App">
+      
+        <h1> RECUERDA NO OLVIDAR</h1>
+      
+      <div className='tarea-lista-principal'>
+        <h2>Mis Tarea</h2>
+        <ListaDeTareas  />
+        </div>
+        <h2></h2>
+    </div>
+  );
+}
+
+export default App;
