223765aaea5380f17251aa09139bcf0262912bf6
fede-cid <fede.cid.96@gmail.com>
2022-09-29 10:42:35 -0300

    first commit

--- app-de-tareas/src/App.js
+++ app-de-tareas/src/App.js
@@ -1,21 +1,24 @@
 import './App.css';
 import ListaDeTareas from './js/listaDeTareas'
-
+import Fade from 'react-reveal/Fade';
 
 function App() {
   return (
+    
     <div className="App">
-      
+      <Fade bottom cascade>
         <h1> RECUERDA NO OLVIDAR</h1>
-      
+     </Fade>
       <div className='tarea-lista-principal'>
+       
         <h2>Mis Tarea</h2>
-       
-        <ListaDeTareas  />
         
-        </div>
+        <ListaDeTareas  />
        
+        </div>
+        
     </div>
+    
   );
 }
 
